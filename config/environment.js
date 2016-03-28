23536eb975deaf82dbb2e956f4ce21aaa3643d58
jared beckler and michael lor <jared.beckler@gmail.com>
2016-03-28 09:09:08 -0700

    add firebase functionality to app

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'super-rentals',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://superrentalsember.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
