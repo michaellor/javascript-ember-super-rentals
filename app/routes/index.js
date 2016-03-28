1de9978322e14158371f0f9ad6cec6b327f1b4c5
jared beckler and michael lor <jared.beckler@gmail.com>
2016-03-28 14:08:04 -0700

    add commented functionality for displaying multipule models on one route

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('rental');
+    return this.store.findAll('rental'),
   },
 
+  // USED IN APP/TEMPLATES/INDEX.HBS FILE TO DISPLAY MULIPULE MODELS ON A PAGE
+  // model() {
+  //   return Ember.RSVP.hash({
+  //     rentals: this.store.findAll('rental'),
+  //     announcements: this.store.findAll('announcement')
+  //   });
+  // },
   actions: {
     save3(parameters) {
       var newRental = this.store.createRecord('rental', parameters);
