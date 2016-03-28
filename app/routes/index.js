29f8bc0e79f47a2306355a020cf34be23c243011
jared beckler and michael lor <Guest@epicodus-115.local>
2016-03-28 15:16:36 -0700

    add update functionality

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('rental'),
+    return this.store.findAll('rental')
   },
 
   // USED IN APP/TEMPLATES/INDEX.HBS FILE TO DISPLAY MULIPULE MODELS ON A PAGE
@@ -18,6 +18,17 @@ export default Ember.Route.extend({
       newRental.save();
       this.transitionTo('index');
     },
+
+    update(rental, parameters) {
+      Object.keys(parameters).forEach(function(key) {
+        if(parameters[key]!==undefined) {
+          rental.set(key,parameters[key]);
+        }
+      });
+      rental.save();
+      this.transitionTo('index');
+    },
+
     destroyRental(rental) {
       rental.destroyRecord();
       this.transitionTo('index');
