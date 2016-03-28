426e800fd1bd953d9eac3782e7e0b0ab40eb0314
jared beckler and michael lor <jared.beckler@gmail.com>
2016-03-28 11:52:31 -0700

    add functionality for deleting announcements

--- app/routes/announcements.js
+++ app/routes/announcements.js
@@ -4,4 +4,10 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('announcement');
   },
+  actions: {
+    destroyAnnouncement(announcement) {
+      announcement.destroyRecord();
+      this.transitionTo('announcement');
+    }
+  }
 });
