7c0a42f748fee8f2c75ade0ddc264c047e6a539b
jared beckler and michael lor <jared.beckler@gmail.com>
2016-03-28 10:56:20 -0700

    add functionality for adding a rental listing and saving in firebase

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,6 +6,11 @@ export default Ember.Route.extend({
   },
 
   actions: {
+    save3(parameters) {
+      var newRental = this.store.createRecord('rental', parameters);
+      newRental.save();
+      this.transitionTo('index');
+    },
     destroyRental(rental) {
       rental.destroyRecord();
       this.transitionTo('index');
